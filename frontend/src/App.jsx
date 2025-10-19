12754b9c2a64f73f9ba1f4ddba7c2220aa1be0d5
Local Epsilon User <localepsilon@cis0678>
2025-10-19 13:48:26 -0700

    app.jsx unstyled, form, taskList object, hardcoded object

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 
+const hardCodedtasks = [
+  { name: 'This is One', completed: true, id: "abc"}
+]
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState(hardCodedtasks);
+
+  function addTask (item) {
+    const newTask = { name: item, completed: false};
+    setTasks([...tasks, newTask])
+  }
+  const taskList = tasks.map((item) => {
+    return (
+      <Todo name={item.name} completed={item.completed}/>
+    )
+  });
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Form />
+    <Todo name="This is hardcoded" completed={false}/>
+    {taskList}
     </>
   )
+  
+}
+function Todo (props) {
+  const [newTodo, setNewTodo] = useState('');
+
+  return (
+    <li>
+      {props.name}
+    </li>
+  )
+}
+
+function Form(props) {
+  const [name, setName] = useState('');
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name.trim()) {
+      return;
+    }
+    props.addTask(name);
+    setName("");
+  }
+
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          What needs to be done?
+        </label>
+      </h2>
+
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+        value={name}
+        onChange={handleChange}
+      />
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+    </form>
+  );
 }
 
 export default App
