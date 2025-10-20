3527c4e0dbb486691143563a5dbc11ebd1cc60c9
Local Epsilon User <localepsilon@cis0678>
2025-10-19 19:47:32 -0700

    app.jsx add/delete/edit implemented with editin/view template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,28 +2,28 @@ import { useState, useRef, useEffect } from 'react'
 import './App.css'
 
 const hardCodedtasks = [
-  { name: 'This is One', completed: true, id: "abc"}
+  { name: 'This is One', completed: true, id: "abc" }
 ]
 
 
 function App() {
   const [tasks, setTasks] = useState(hardCodedtasks);
 
-  function addTask (item) {
+  function addTask(item) {
     const m = Math.floor(Math.random() * 9999 + 1000);
-    const newTask = { name: item, completed: false, id:'abc'+ m};
+    const newTask = { name: item, completed: false, id: 'abc' + m };
     setTasks([...tasks, newTask])
   }
 
-  function deleteTask (idDel) {
-    const afterDel = tasks.filter( item => item.id != idDel);
+  function deleteTask(idDel) {
+    const afterDel = tasks.filter(item => item.id != idDel);
     setTasks(afterDel)
   }
 
-  function editTask (idEd, newName){
+  function editTask(idEd, newName) {
     let updatedTask;
-    const editedTaskList = tasks.map( task => {
-      if (task.id === idEd){
+    const editedTaskList = tasks.map(task => {
+      if (task.id === idEd) {
         updatedTask = { ...task, name: newName }
         return updatedTask;
       }
@@ -34,31 +34,94 @@ function App() {
 
   const taskList = tasks.map((item) => {
     return (
-      <Todo key={item.id}  name={item.name} completed={item.completed} id={item.id}  deleteTask={deleteTask} editTask={editTask} />
+      <Todo key={item.id} name={item.name} completed={item.completed} id={item.id} deleteTask={deleteTask} editTask={editTask} />
     )
   });
 
-
   return (
     <>
-    <Form addTask={addTask} />
-    <Todo name="This is hardcoded" completed={false} id="cde" />
-    {taskList}
+      <Form addTask={addTask} />
+      <Todo name="This is hardcoded" completed={false} id="cde" />
+      {taskList}
     </>
   )
-  
 }
-function Todo (props) {
-  const [newTodo, setNewTodo] = useState('');
+function Todo(props) {
+  const [isEditing, setEditing] = useState(false);
+  const [newName, setNewName] = useState('');
 
-  return (
-    <li>
-      {props.name} 
-      <button onClick={() => props.deleteTask(props.id)}>DELETE</button>
-      {/* {props.name} <button onClick={() => alert(props.id)}>DELETE</button> */}
-      <button onClick={() => props.editTask(props.id, props.name)}>EDIT</button>
-    </li>
-  )
+  function handleChange(e) {
+    setNewName(e.target.value);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.editTask(props.id, newName);
+    setNewName("");
+    setEditing(false);
+  }
+
+  const editingTemplate = (
+    <form className="stack-small" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="todo-label" htmlFor={props.id}>
+          New name for {props.name}
+        </label>
+        <input
+          id={props.id}
+          className="todo-text"
+          type="text"
+          value={newName || props.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="btn-group">
+        <button
+          type="button"
+          className="btn todo-cancel"
+          onClick={() => setEditing(false)}
+        >
+          Cancel
+          <span className="visually-hidden">renaming {props.name}</span>
+        </button>
+        <button type="submit" className="btn btn__primary todo-edit">
+          Save
+          <span className="visually-hidden">new name for {props.name}</span>
+        </button>
+      </div>
+    </form>
+  );
+
+  const viewTemplate = (
+    <div className="stack-small">
+      <div className="c-cb">
+        <label className="todo-label" htmlFor={props.id}>
+          {props.name}
+        </label>
+      </div>
+      <div className="btn-group">
+        <button
+          type="button"
+          className="btn"
+          onClick={() => setEditing(true)}
+        >
+          Edit <span className="visually-hidden">{props.name}</span>
+        </button>
+        <button
+          type="button"
+          className="btn btn__danger"
+          onClick={() => {
+            console.log(props.id)
+            props.deleteTask(props.id)
+          }
+          }
+        >
+          Delete <span className="visually-hidden">{props.name}</span>
+        </button>
+      </div>
+    </div>
+  );
+
+ return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 }
 
 function Form(props) {
